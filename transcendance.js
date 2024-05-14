a988c060268361e4143ebaf3b01fbbc5b2b165ca
Jordan MPB <contact@jordan-chap.com>
2024-05-15 01:32:28 +0200

    start

--- transcendance.js
+++ transcendance.js
@@ -1,25 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 
-// Import required modules
-
-// Create an Express application
 const app = express();
 
-// Set the views directory and view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'test_pongjs')));
 
 // Define routes
 app.get('/', (req, res) => {
-	res.render('index');
+	res.send(200).sendFile(path.join(__dirname, 'test_pongjs', 'index.html'));
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
