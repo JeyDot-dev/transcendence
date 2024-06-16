770a4b69789bd58ec8472908c4b49e6c0a0ed6a5
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-06-16 16:26:00 +0000

    Basic working config of django with postgre

--- django/script.sh
+++ django/script.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Check if Django project exists
+sleep 5
 if [[ ! -e /project/manage.py ]]; then
 	django-admin startproject project .
+	mv /etc/settings.py /project/project/settings.py
+	django-admin migrate
 else
 	echo "Django project already exists."
 fi
