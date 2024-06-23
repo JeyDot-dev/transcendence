1bd6b1781ee278abda57377f603cafa94450c7c3
R4yL <luca.ray@protonmail.ch>
2024-06-23 18:26:48 +0200

    add middleware

--- django/project/static/js/app.js
+++ django/project/static/js/app.js
@@ -1,66 +1,71 @@
 const navigateTo = url => {
-    history.pushState(null, null, url);
-    router();
+	history.pushState(null, null, url);
+	router();
 };
 
 const router = async () => {
-    const routes = [
-        { path: "/", view: "/api/home/" },
-        { path: "/pong", view: "/api/pong/" },
-        { path: "/pong/game", view: "/api/pong/game/" },
-        { path: "/about", view: "/api/about/" },
-        { path: "/test", view: "/api/test/" }
-    ];
+	const routes = [
+		{ path: "/", view: "/api/home/" },
+		{ path: "/pong", view: "/api/pong/" },
+		{ path: "/pong/game", view: "/api/pong/game/" },
+		{ path: "/about", view: "/api/about/" },
+		{ path: "/test", view: "/api/test/" }
+	];
 
-    const path = location.pathname.endsWith("/") && location.pathname.length > 1
-        ? location.pathname.slice(0, -1)
-        : location.pathname;
+	const path = location.pathname.endsWith("/") && location.pathname.length > 1
+		? location.pathname.slice(0, -1)
+		: location.pathname;
 
-    const potentialMatches = routes.map(route => {
-        return {
-            route: route,
-            isMatch: path === route.path
-        };
-    });
+	const potentialMatches = routes.map(route => {
+		return {
+			route: route,
+			isMatch: path === route.path
+		};
+	});
 
-    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+	let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
 
-    if (!match) {
-        match = {
-            route: routes[0],
-            isMatch: true
-        };
-    }
+	if (!match) {
+		match = {
+			route: routes[0],
+			isMatch: true
+		};
+	}
 
-    const html = await fetch(match.route.view).then(response => response.text());
-    const mainElement = document.querySelector("main");
-    mainElement.innerHTML = html;
+	const html = await fetch(match.route.view, {
+		headers: {
+			'X-Requested-With': 'XMLHttpRequest'
+		}
+	}).then(response => response.text());
 
-    // Exécuter les scripts manuellement après l'insertion du HTML
-   const scripts = mainElement.querySelectorAll('script');
-    scripts.forEach(script => {
-        const newScript = document.createElement('script');
-        if (script.src) {
-            newScript.src = script.src;
-        } else {
-            // Wrap the script content in an IIFE
-            newScript.textContent = `(function() { ${script.textContent} })();`;
-        }
-        document.head.appendChild(newScript).parentNode.removeChild(newScript);
-    });
+	const mainElement = document.querySelector("main");
+	mainElement.innerHTML = html;
+
+	// Exécuter les scripts manuellement après l'insertion du HTML
+	const scripts = mainElement.querySelectorAll('script');
+	scripts.forEach(script => {
+		const newScript = document.createElement('script');
+		if (script.src) {
+			newScript.src = script.src;
+		} else {
+			// Wrap the script content in an IIFE
+			newScript.textContent = `(function() { ${script.textContent} })();`;
+		}
+		document.head.appendChild(newScript).parentNode.removeChild(newScript);
+	});
 };
 
 window.addEventListener("popstate", router);
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.addEventListener("click", e => {
-        if (e.target.matches("[data-link]")) {
-            e.preventDefault();
-            if (e.target.href !== window.location.href) {
-                navigateTo(e.target.href);
-            }
-        }
-    });
+	document.body.addEventListener("click", e => {
+		if (e.target.matches("[data-link]")) {
+			e.preventDefault();
+			if (e.target.href !== window.location.href) {
+				navigateTo(e.target.href);
+			}
+		}
+	});
 
-    router();
+	router();
 });
--- django/project/transcendence/settings.py
+++ django/project/transcendence/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+	"transcendence.middleware.FetchOnlyMiddleware",
 ]
 
 ROOT_URLCONF = "transcendence.urls"
