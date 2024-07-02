9ccbde68d55632a63c3f2f8e589387cd3ad72bb7
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-07-02 20:39:59 +0000

    Moved remaining files for the merge prod-mode isn't serving static files right now has expected, I'll fix this tomorrow, dev-mode is fully usable though

--- django-settings/prod-settings.py
+++ django-settings/prod-settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "transcendence.middleware.FetchOnlyMiddleware",
 ]
 
 ROOT_URLCONF = "transcendence.urls"
