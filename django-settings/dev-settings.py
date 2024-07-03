6a97e8f50a74d51d8d29aa48b72607dd5acef9a1
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-07-03 12:56:02 +0000

    Kinda merged Db_tournament_wip (added database app folder)

--- django-settings/dev-settings.py
+++ django-settings/dev-settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "database.apps.DatabaseConfig",
     "channels",
     "pong",
 ]
@@ -52,7 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-	"transcendence.middleware.FetchOnlyMiddleware",
+    "transcendence.middleware.FetchOnlyMiddleware",
 ]
 
 ROOT_URLCONF = "transcendence.urls"
---
+++ project/database/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('play/<int:game_id>/', views.play, name='play'),
+    path('winner/<int:game_id>/', views.winner, name='winner'),
+    path('play/', views.newGame, name='newGame'),
+    path('newTournament/', views.newTournament, name = 'newTournament'),
+    path('addPlayers/<int:t_id>/', views.addPlayers, name = 'addPlayers'),
+    path('startTournament/<int:t_id>/', views.startTournament, name = 'startTournament'),
+    path('tournamentWinner/<int:t_id>/', views.tournamentWinner, name = 'tournamentWinner'),
+]
