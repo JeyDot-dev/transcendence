ed6bbb2bed99ca71f65655e39dbd71564e852a75
R4yL <luca.ray@protonmail.ch>
2024-07-05 00:04:48 +0200

    delete useless settings

--- django-settings/dev-settings.py
+++ django-settings/dev-settings.py
@@ -141,7 +141,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static_production")
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "pong/static/pong",
-    BASE_DIR / "database/static/database",
 ]
 
 
