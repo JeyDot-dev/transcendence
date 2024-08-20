0e5e72ef2d2734489fe241f84c57b977d0340108
R4yL <luca.ray@protonmail.ch>
2024-08-20 17:02:18 +0200

    fix how SPA handle "/" in URLs

--- project/transcendence/urls.py
+++ project/transcendence/urls.py
@@ -21,10 +21,10 @@ from . import views
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path("api/home", views.home_content, name="home_content"),
-    path("api/pong", include("database.urls")),
-    path("api/about", views.about_content, name="about_content"),
-    path("api/test", views.test_content, name="test_content"),
+    path("api/home/", views.home_content, name="home_content"),
+    path("api/pong/", include("database.urls")),
+    path("api/about/", views.about_content, name="about_content"),
+    path("api/test/", views.test_content, name="test_content"),
     re_path(r"^api/.*$", views.home_content),
     re_path(r"^.*$", views.index, name="index"),
 ]
