8cc16d0c0f94f051f0db04c3d15da5e4d3076afd
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-09-10 16:49:28 +0000

    Started moving navbar for autorefresh without reloading

--- project/transcendence/urls.py
+++ project/transcendence/urls.py
@@ -21,13 +21,14 @@ from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path("admin", admin.site.urls),
     path("api/home/", views.home_content, name="home_content"),
     # path("api/pong/", include("database.urls")),
     path("api/pong/", include("pong.urls")),
     path("api/userManager/", include("userManager.urls")),
     path("api/about/", views.about_content, name="about_content"),
     path("api/test/", views.test_content, name="test_content"),
+    path("api/navbar/", include("navbar.urls")),
     re_path(r"^api/.*$", views.default_content, name="default_content"),
     re_path(r"^.*$", views.index, name="index"),
 ]
