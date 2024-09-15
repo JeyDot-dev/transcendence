00a84d927aa73fb911d7fd33086449322f0b6020
Forgotw <116347457+Forgotw@users.noreply.github.com>
2024-09-15 17:05:38 +0200

    fix modal

--- project/pong/static/pong/Classes/menu.js
+++ project/pong/static/pong/Classes/menu.js
@@ -183,7 +183,7 @@ export class Menu {
     // TODO: hauteur du canvas 
     createMenuItems() {
         this.localMenuMain = new MenuItem(this.menuGroup, this.scene, this.camera, this.font, 'Local', this.colorPalette[0], new THREE.Vector3(0, 0, 380), () => {
-            this.formSubmittedSuccessfully = false;
+            this.formSubmittedSuccessfully = false; 
             this.disableEventListener();
             this.modalManager.openModal('modalNewGame', this.handleNewGameSubmit.bind(this), this);
             //this.newLocalGame();
